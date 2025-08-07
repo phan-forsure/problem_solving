d2c8fd862bff9948e8d72a34eded89a3e8d71020
phan-forsure <phantommy38@gmail.com>
2025-08-07 15:03:48 +0300

    added practice dir

---
+++ cpp_codeforces/pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void make_scentence_uppercase(string * name)
+{
+  for (int i = 0; i < name.size(); i++)
+  {
+    *name[i] = *name[i]
+  }
+}
+
+int main()
+{
+  string name = "hello world";
+
+  return 0;
+}
